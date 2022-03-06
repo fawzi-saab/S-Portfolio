86dd41f49c103f59c8212286bd2d2d0d708ec77b
fawzisaab <reddevil_ut@hotmail.com>
2022-03-06 18:55:17 +0000

    h

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -2,6 +2,5 @@ class Certificate < ActiveRecord::Base
     belongs_to :profile
     has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "https://www.freeiconspng.com/uploads/no-image-icon-11.PNG"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  validates :title, presence: true
-    validates :institute, presence: true
+  
 end
--- app/models/education.rb
+++ app/models/education.rb
@@ -1,6 +1,4 @@
 class Education < ActiveRecord::Base
     belongs_to :profile
-    validates :title, presence: true
-    validates :institute, presence: true
-    validates :year_of_graduation, presence: true
+   
 end
